fixed a few bugs with normalization, loss makes sense now

src/card_rec_system.py
@@ -33,7 +33,9 @@ class DemographicModel(nn.Module):
     def __init__(self, demographic_dim: int, latent_dim: int):
         super(DemographicModel, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(demographic_dim, latent_dim)
+            nn.Linear(demographic_dim, latent_dim),
+            nn.BatchNorm1d(latent_dim),
+            nn.ReLU()
         )
     
     def forward(self, x: Tensor) -> Tensor:
@@ -52,6 +54,8 @@ class UserLatentNet(nn.Module):
     def forward(self, x: Tuple[Tensor, Tensor]) -> Tensor:
         ud = self.demographicModel(x[0])
         ut = self.transactionModel(x[1])
+        #print("Demographic output: ", ud)
+        #print("Transaction output: ", ut)
         ul = self.latentModel(torch.concat((ud, ut), dim=-1))
         return ul
 
@@ -84,8 +88,10 @@ class RecommendationSystem(nn.Module):
 
     def forward(self, x: Tuple[Tuple[Tensor, Tensor], Tensor]) -> Tensor:
         user_latent = self.userLatentModel(x[0])
+        #print("user_latent: ", user_latent)
         item_indices = x[1].squeeze(-1)
         item_latent = self.itemEmbeddings(item_indices)
+        #print("item_latent: ", item_latent)
         score = self.NCF(torch.concat((user_latent, item_latent), dim=-1))
         return score
     
@@ -97,10 +103,12 @@ class RecommendationSystem(nn.Module):
             
             for batch in tqdm(train_data):
                 user_input, item_input, label = batch
+                #print("Demographic Input: ", user_input[0])
                 predicted_score = self((user_input, item_input))
                 
                 optimizer.zero_grad()
                 loss = loss_fn(predicted_score.squeeze(-1), label.float())
+                print(loss)
                 loss.backward()
                 optimizer.step()
                 

src/utils/preprocessing.py
@@ -109,8 +109,8 @@ class Preprocessing:
                 aggregated_transaction = self.default_agg_transaction.copy()
                 mcc_amounts = {}
                 start_date = self.get_date(transaction["Year"],transaction["Month"],transaction["Day"])
-            amount = self.convert_currency(transaction["Amount"])
-            mcc_amounts[transaction["MCC"]] = mcc_amounts.get(transaction["MCC"], 0) + amount
+            #amount = self.convert_currency(transaction["Amount"])
+            mcc_amounts[transaction["MCC"]] = mcc_amounts.get(transaction["MCC"], 0) + transaction["Amount"]
             aggregated_transaction["Average_Zip"] += transaction["Zip"]
             aggregated_transaction["Max_Zip"] = max(aggregated_transaction["Max_Zip"],
                                                     transaction["Zip"])
@@ -147,12 +147,7 @@ class Preprocessing:
 
         gender = demographic_info["Gender"]
         demographic_info["Gender"] = 1 if gender =="Male" else 0
-
-        for feature in ["Per Capita Income - Zipcode",
-                        "Yearly Income - Person",
-                        "Total Debt"]:
-            demographic_info[feature] = self.convert_currency(demographic_info[feature])
-
+        
         demographic_data = torch.tensor(list(map(demographic_info.get, self.demographic_fields)),
                                         dtype=torch.float32)
 
@@ -164,6 +159,15 @@ class Preprocessing:
     def prepare_dataset(self, df: pd.DataFrame):
         """Replaces nan values with their mode for categorical columns and nan values with their mean for numerical columns"""
         
+        currency_cols = ["Per Capita Income - Zipcode",
+                         "Yearly Income - Person",
+                         "Total Debt",
+                         "Amount"]
+        
+        for col in currency_cols:
+            if col in df.columns:
+                df[col] = df[col].apply(self.convert_currency)
+
         categorical_cols = df.select_dtypes(include=['object']).columns
         numerical_cols = df.select_dtypes(include=['number']).columns
 